667a95077bdd4c76fe3759f339d0f56888961816
lnormanbello <lnorman2682@gmail.com>
2024-11-16 11:31:05 -0600

    Actualización de catalogo Evaluator

---
+++ UI/EvaluatorView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MascotContest.UI
+{
+    public partial class EvaluatorView : Form
+    {
+        public EvaluatorView()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
