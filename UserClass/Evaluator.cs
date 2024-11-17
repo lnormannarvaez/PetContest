13e174926d27fc5d58078d22413655e03297fd6f
lnormanbello <lnorman2682@gmail.com>
2024-11-16 19:30:45 -0600

    Desarrollo de Catalogo de Evaluador

---
+++ UserClass/Evaluator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MascotContest.UserClass
+{
+    internal class Evaluator
+    {
+        public string Code { get; set; }
+        public string Names { get; set; }
+        public string Suraname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
